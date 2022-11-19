49b996b10d2ee75808107a122138476859bff2ac
Farmaan <mfmohamed@students.nsbm.ac.lk>
2022-11-19 22:17:32 +0530

    Update coursework_2.m
    Added mean performance using Silhouette, plotted the
    Silhouette for each cluster

--- coursework_2.m
+++ coursework_2.m
@@ -1,42 +1,45 @@
-% Task 2
+clear; close all; clc;
 
-% Task 2.1 - Data Preperation
+%% Task 2.1 - Data Preperation
 X = gen_kmeansdata(10749829);
 
+% Reporting number of rows, mean, standard deviation for each column
 N = size(X,1);
 M = mean(X);
 S = std(X);
 
+% Reporting covariance and correlation matrices (4x4)
+cov_matrix = cov(X);
+cor_matrix = corrcoef(X);
+
+% Displaying histogram
 histogram(X)
 
-% Task 2.2 - K Means
-% for K = [3, 4, 5]
-%     [idx,C,sumd]=kmeans(X,K);
-%     [silh,h] = silhouette(X,idx,'sqEuclidean');
-%     title(['Number of clussters = ' int2str(K)]);
-%     xlabel 'Silhouette Value '
-%     ylabel 'Cluster'
-%     mean_silh=mean(silh);
-% end
+%% Task 2.2 - K Means
+for K = [3, 4, 5]
+    figure(K-1)
+    [idx,C,sumd] = kmeans(X,K);
+    [silh,h] = silhouette(X, idx, 'sqEuclidean');
+    title(['Number of Clusters = ' int2str(K)]);
+    xlabel 'Silhouette Value '
+    ylabel 'Cluster'
+    mean_silh = mean(silh);
+end
 
-K = 3;
-[idx,C,sumd]=kmeans(X,K);
-[silh,h] = silhouette(X,idx,'sqEuclidean');
-title(['Number of clussters = ' int2str(K)]);
-xlabel 'Silhouette Value '
-ylabel 'Cluster'
-mean_silh=mean(silh);
-
-figure;
-plot(X(idx==1,1),X(idx==1,2),'r.','MarkerSize',12)
+figure(5);
+plot(X(idx==1,1), X(idx==1,2), 'r.', 'MarkerSize', 12)
 hold on
-plot(X(idx==2,1),X(idx==2,2),'b.','MarkerSize',12)
+plot(X(idx==2,1), X(idx==2,2), 'b.', 'MarkerSize', 12)
 hold on
-plot(X(idx==3,1),X(idx==3,2),'g.','MarkerSize',12)
+plot(X(idx==3,1), X(idx==3,2), 'g.', 'MarkerSize', 12)
+hold on
+plot(X(idx==4,1), X(idx==4,2), 'c.', 'MarkerSize', 12)
+hold on
+plot(X(idx==5,1), X(idx==5,2), 'm.', 'MarkerSize', 12)
 
-plot(C(:,1),C(:,2),'kx', 'MarkerSize',15,'LineWidth',3) 
-legend('Cluster 1','Cluster 2','Cluster 3','Centroids', 'Location','NW')
-title 'Cluster Assignments and Centroids'
+plot(C(:,1), C(:,2), 'kx', 'MarkerSize', 15, 'LineWidth', 3) 
+legend('Cluster 1', 'Cluster 2', 'Cluster 3', 'Cluster 4', 'Cluster 5', 'Centroids', 'Location', 'NW')
+title 'Clusters and Centroids'
 hold off
 
 
