83e1f3822094ba7145787083668a66b43946c50e
Farmaan <mfmohamed@students.nsbm.ac.lk>
2022-12-22 20:21:44 +0530

    Update coursework_2.m
    Updated Cluster and Centroid plot, with reporting the best cluster

--- coursework_2.m
+++ coursework_2.m
@@ -1,5 +1,8 @@
 clear; close all; clc;
 
+%% Name: Mohamed Mohamed
+% Plymouth Index: 10749829
+
 %% Task 2.1 - Data Preperation
 % Generating data
 X = gen_kmeansdata(10749829);
@@ -10,7 +13,7 @@ M = mean(X);
 S = std(X);
 
 % Displaying histogram
-histogram(X)
+histogram(X);
 
 % Reporting covariance and correlation matrices (4x4)
 cov_matrix = cov(X);
@@ -18,7 +21,7 @@ cor_matrix = corrcoef(X);
 
 %% Task 2.2 - K Means
 % Assigning empty array to append mean Silhouettes for K=3,4,5
-mean_silh = [];
+mean_silh_array = [];
 
 % Iterating to assign different cluster values
 for K = [3, 4, 5]
@@ -27,7 +30,7 @@ for K = [3, 4, 5]
     
     % Seperating data into K clusters
     % idx - cluster indexes, C - centroids
-    [idx, C, sumd] = kmeans(X, K);
+    [idx, C, sum] = kmeans(X, K);
     
     % Evaluating quality of cluster
     [silh, h] = silhouette(X, idx, 'sqEuclidean');
@@ -37,25 +40,46 @@ for K = [3, 4, 5]
     xlabel 'Silhouette Value'
     ylabel 'Cluster'
 
-    % For the optimum K, the mean Silhouette must be closer to 1
-    mean_silh(end+1) = mean(silh);
+    % Appending the mean Silhoette to an array
+    mean_silh = mean(silh);
+    mean_silh_array(end+1) = mean_silh;
+
+    % Plotting the Cluster and Centroids for K=5
+    figure(K+2);
+
+    plot(X(idx == 1, 1), X(idx == 1, 2), 'r.', 'MarkerSize', 9)
+    hold on
+    plot(X(idx == 2, 1), X(idx == 2, 2), 'g.', 'MarkerSize', 9)
+    hold on
+    plot(X(idx == 3, 1), X(idx == 3, 2), 'b.', 'MarkerSize', 9)
+    hold on
+    plot(X(idx == 4, 1), X(idx == 4, 2), 'm.', 'MarkerSize', 9)
+    hold on
+    plot(X(idx == 5, 1), X(idx == 5, 2), 'c.', 'MarkerSize', 9)
     
+    plot(C(:, 1), C(:, 2), 'kx', 'MarkerSize', 10, 'LineWidth', 2) 
+    
+    if K == 3
+        legend('Cluster 1', 'Cluster 2', 'Cluster 3', 'Centroids', 'Location', 'NW')
+    elseif K == 4
+        legend('Cluster 1', 'Cluster 2', 'Cluster 3', 'Cluster 4', 'Centroids', 'Location', 'NW')
+    elseif K == 5
+        legend('Cluster 1', 'Cluster 2', 'Cluster 3', 'Cluster 4', 'Cluster 5', 'Centroids', 'Location', 'NW')
+    end
+
+    title([int2str(K) ' Clusters and Centroids']);
+    hold off
+
+    disp(['K = ', num2str(K), '; The mean Silhouette measure = ', num2str(mean_silh)])
+
 end
 
-% figure(5);
-% plot(X(idx==1,1), X(idx==1,2), 'r.', 'MarkerSize', 8)
-% hold on
-% plot(X(idx==2,1), X(idx==2,2), 'g.', 'MarkerSize', 8)
-% hold on
-% plot(X(idx==3,1), X(idx==3,2), 'b.', 'MarkerSize', 8)
-% hold on
-% plot(X(idx==4,1), X(idx==4,2), 'c.', 'MarkerSize', 8)
-% hold on
-% plot(X(idx==5,1), X(idx==5,2), 'm.', 'MarkerSize', 8)
-% 
-% plot(C(:,1), C(:,2), 'kx', 'MarkerSize', 15, 'LineWidth', 3) 
-% legend('Cluster 1', 'Cluster 2', 'Cluster 3', 'Cluster 4', 'Cluster 5', 'Centroids', 'Location', 'NW')
-% title 'Clusters and Centroids'
-% hold off
+% Reporting best number of clusters
+mean_silh_dict = dictionary(mean_silh_array, [3, 4, 5]);
+
+% For the optimum K, the mean Silhouette must be closer to 1
+max_mean_silh = max(mean_silh_array);
+best_cluster = mean_silh_dict(max_mean_silh);
+disp(['The best number of Clusters = ', num2str(best_cluster)]);
 
 
